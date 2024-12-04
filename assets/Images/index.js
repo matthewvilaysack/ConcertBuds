c47395ffd1416be183b8cfa30c91d1a780652d93
sarahlstt <sarahlst@stanford.edu>
2024-12-03 22:09:23 -0800

    navbar active/inactive implemented

--- assets/Images/index.js
+++ assets/Images/index.js
@@ -1,9 +1,13 @@
 const Images = {
   background: require("./background.png"),
-  home: require("./home.png"),
+  homeActive: require("./homeActive.png"),
+  homeInactive: require("./homeInactive.png"),
   chat_icon: require("./chat_icon.png"),
-  map: require("./map.png"),
+  chatActive: require("./chatActive.png"),
+  mapInactive: require("./mapInactive.png"),
+  mapActive: require("./mapActive.png"),
   profile_icon: require("./profile_icon.png"),
+  profileActive: require("./profileActive.png"),
   members_icon: require("./members-icon.png"), // Added new image
 };
 
