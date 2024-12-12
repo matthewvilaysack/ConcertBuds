6378dfe3a9ac4c337961f25a756dcc081b89b0f1
Matthew Vilaysack V2 <matthewvilaysackv2@Matthews-MacBook-Pro-85.local>
2024-12-12 11:17:53 -0800

    SUPA DUPA

--- utils/env.js
+++ utils/env.js
@@ -1,8 +1,9 @@
-// env.js
 const ENV = {
   TICKETMASTER_API_KEY: "hq67ZSA9zbMrsUmm53qkrofaq0wOKTmq",
   BASE_URL: "https://app.ticketmaster.com/discovery/v2/events.json",
   MAPS_API_KEY: "AIzaSyDWDGpqVjj5dgDRNqWC8LuXcAEgPqbP8KI",
-};
-
-export default ENV;
+  SUPABASE_URL: "https://mdaofsauhonoqzdzmazx.supabase.co";
+  SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kYW9mc2F1aG9ub3F6ZHptYXp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI2NjE4MDksImV4cCI6MjA0ODIzNzgwOX0.83S7c4VhzcG8hhS8qi2Z5EukqDH_l9Q8421yNIbJOFM";
+ };
+ 
+ export default ENV;
